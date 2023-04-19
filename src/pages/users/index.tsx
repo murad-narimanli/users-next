586a3a1135651effc79dc277beea430cc6a4ac36
murad-narimanli <murad.narimanli@mail.ru>
2023-04-19 19:30:53 +0400

    changes

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -98,7 +98,7 @@ export default function Users(props:AboutProps) {
                             </div>
                         </div>
                     </Link>
-                </>
+                </React.Fragment>
            ))}
         </div>
         
