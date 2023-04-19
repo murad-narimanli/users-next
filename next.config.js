4a7d782b79051e39b28a72281deb9cefef8bd254
murad-narimanli <murad.narimanli@mail.ru>
2023-04-19 19:39:41 +0400

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://reqres.in/'],
+  },
 }
+
 
 module.exports = nextConfig
